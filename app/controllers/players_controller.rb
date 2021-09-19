d3c0bb02392e6961e642f11fa024c35182a00b4e
David Colby <davidlcolby@gmail.com>
2021-09-19 15:27:27 -0500

    Set root route and cleanup initial query

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,7 +3,7 @@ class PlayersController < ApplicationController
 
   # GET /players
   def index
-    @players = Player.all
+    @players = Player.includes(:team)
   end
 
   def list
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
       get 'list'
     end
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "players#index"
 end
